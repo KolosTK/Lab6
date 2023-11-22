39f98854440dfeab354d1991acfe99dfcff6ee76
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-22 12:07:26 +0200

    *Create Class Cinema *Create private array with all places in hall *Add methods bookSears, cancelBooking, checkAvailability and printSeatingArrangement

---
+++ src/main/java/org/example/Cinema.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Cinema {
+
+    private int [][][] places = new int [5][10][20];
+    public void bookSeats(int hallNumber, int row, int[] seats)
+    {
+
+    }
+    public void cancelBooking(int hallNumber, int row, int[] seats)
+    {
+
+    }
+    public void checkAvailability(int screen, int numSeats)
+    {
+
+    }
+    public void printSeatingArrangement(int hallNumber)
+    {
+
+    }
+}
