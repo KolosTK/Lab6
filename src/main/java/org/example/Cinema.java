f5d670dcc3ae18f9bf766f61fc78eb438f68dcfd
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-22 14:42:34 +0200

    Change output background color of reserved place from black to yellow

--- src/main/java/org/example/Cinema.java
+++ src/main/java/org/example/Cinema.java
@@ -7,6 +7,7 @@ public class Cinema {
     private int[][][] places = new int[5][10][20];
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String RESET = "\033[0m";
 
@@ -42,7 +43,6 @@ public class Cinema {
 
     public void printSeatingArrangement(int hallNumber) {
         int indexOfRow = 1;
-
         printSearingColumns(hallNumber);
 
         for (int i = 0; i < places[hallNumber].length; i++) {
@@ -52,7 +52,7 @@ public class Cinema {
             System.out.print(indexOfRow + " |");
             for (int j = 0; j < places[hallNumber][i].length; j++) {
                 if (places[hallNumber][i][j] != 0) {
-                    System.out.print(ANSI_RED + ANSI_BLACK_BACKGROUND + " " + places[hallNumber][i][j] + " " + RESET);
+                    System.out.print(ANSI_RED + ANSI_YELLOW_BACKGROUND + " " + places[hallNumber][i][j] + " " + RESET);
 
                 } else {
                     System.out.print(ANSI_GREEN + ANSI_BLACK_BACKGROUND + " " + places[hallNumber][i][j] + " " + RESET);
