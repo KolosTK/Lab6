e4f239610611b47af91b857fea73f1eb32626f83
KolosTK <94901694+KolosTK@users.noreply.github.com>
2023-11-26 18:32:06 +0200

    Create tests for checkAvailability method

---
+++ src/test/java/org/example/checkAvailabilityTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class checkAvailabilityTest {
+    Cinema cinema = new Cinema();
+
+    @Test
+    void testCheckAvailabilitySuccessful() {
+        assertTrue(cinema.checkAvailability(3, 5));
+    }
+
+    @Test
+    void testCheckAvailabilityUnsuccessful() throws SelectingSeatsException {
+        int[] seats = {3, 4};
+        for (int i = 1; i <= cinema.get_rowsAmount(); i++) {
+            cinema.bookSeats(2, i, seats);
+        }
+        assertFalse(cinema.checkAvailability(2, 18));
+    }
+}
